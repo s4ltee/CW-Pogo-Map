df480eb4f433384c2ddab0e405898823fcd81869
SparklesMaster <67660537+SparklesMaster@users.noreply.github.com>
2022-07-09 23:25:32 -0400

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,7 @@ $(window).on('load', function() {
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
-          (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
+          (point['Image'] ? ('<img src="' + point['Image'] + '"><br> Description') : '') +
           point['Description'] + '<br>' + point['Live Date']);
 
         if (layers !== undefined && layers.length !== 1) {
