40469c7418c1fbe96d8778b81ac52804324fa9b9
s4ltee <67660537+s4ltee@users.noreply.github.com>
2022-07-09 22:14:27 -0400

    API

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/18SIeTRvz_6atCYV1HRPyaNROWBHvdj42kT-ByNUT0r0/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBA5clsEOrmlrDgWHJ-IdlefX2BaLpb85A';
